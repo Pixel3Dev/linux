5555736aedb37e84ba1c81179b4491fbdcd4770b
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-25 16:02:13 -0800

    ok, this reboots, so I guess we weren't allowed to copy the log buffer yet

--- init/main.c
+++ init/main.c
@@ -611,6 +611,7 @@ asmlinkage __visible void __init start_kernel(void)
 	sort_main_extable();
 	trap_init();
 	mm_init();
+	rebootphone();
 
 	ftrace_init();
 
@@ -620,7 +621,6 @@ asmlinkage __visible void __init start_kernel(void)
 	void* alt_ramoops = ioremap(0xa1a10000ULL, 0x200000);
 	memset(alt_ramoops, 'A', 0x200000);
 	memcpy(alt_ramoops, log_buf_addr_get(), min(log_buf_len_get(), 0x200000));
-	rebootphone();
 
 	/*
 	 * Set up the scheduler prior starting any interrupts (such as the
